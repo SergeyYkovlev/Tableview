95ca89febd8d2ae8b6bc09cc1fb3c342778ad8e6
SergeyYkovlev <yakovlevserg09@gmail.com>
2022-01-20 09:11:52 +0600

    creating a cell table

--- Tableview/Tableview/ViewController.swift
+++ Tableview/Tableview/ViewController.swift
@@ -9,24 +9,30 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    
-    var logoImage: [UIImage] = [
-            UIImage(named: "png.png")!,
-            UIImage(named: "splash.png")!,
-            UIImage(named: "big.png")!
-        ]
-    let doc = ["привет", "привет","привет", "привет"]
-    let list = ["Егор", "Ваня", "Петя", "Кирилл", "Сергей", "Ваня", "Маша","Вика", "Руся", "Дима", "Катя", "Игрорь", "Ваня", "Петр", "Ваня", "Петя", "Кирилл", "Сергей", "Ваня", "Маша","Вика", "Руся",]
+    let text = ["png", "big", "splash"]
+    let animals = ["png", "big", "splash"]
+        
+    //let doc = ["привет", "привет","привет", "привет"]
+    //let list = ["Егор", "Ваня", "Петя", "Кирилл", "Сергей", "Ваня", "Маша","Вика", "Руся", "Дима", "Катя", "Игрорь", "Ваня", "Петр", "Ваня", "Петя", "Кирилл", "Сергей", "Ваня", "Маша","Вика", "Руся",]
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return animals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        cell.textLabel?.text = list[indexPath.row]
-        cell.detailTextLabel?.text = list[indexPath.row]
-        cell.imageView?.image = UIImage(named: "png")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+        
+        cell.myImage.image = UIImage(named: animals[indexPath.row])
+        cell.myLabel.text = text[indexPath.row]
+        
+        
+        
+        
+        //let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        //cell.textLabel?.text = list[indexPath.row]
+        //cell.detailTextLabel?.text = list[indexPath.row]
+        //cell.imageView?.image = UIImage(named: "png")
         
         return(cell)
     }
---
+++ Tableview/Tableview/ViewControllerTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ViewControllerTableViewCell.swift
+//  Tableview
+//
+//  Created by Сергей Яковлев on 19.01.2022.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
