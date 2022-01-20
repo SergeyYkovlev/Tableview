3eebf2dd47f8cb54edc02cfa40d2ebf035b37110
SergeyYkovlev <yakovlevserg09@gmail.com>
2022-01-20 09:52:00 +0600

    table

--- Tableview/Tableview/ViewController.swift
+++ Tableview/Tableview/ViewController.swift
@@ -9,40 +9,27 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    let text = ["png", "big", "splash"]
+    let number = ["89994572085", "89513458976", "89605743561"]
+    let text = ["Misha", "Masha", "Sergey"]
     let animals = ["png", "big", "splash"]
         
-    //let doc = ["привет", "привет","привет", "привет"]
-    //let list = ["Егор", "Ваня", "Петя", "Кирилл", "Сергей", "Ваня", "Маша","Вика", "Руся", "Дима", "Катя", "Игрорь", "Ваня", "Петр", "Ваня", "Петя", "Кирилл", "Сергей", "Ваня", "Маша","Вика", "Руся",]
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return animals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
         
         cell.myImage.image = UIImage(named: animals[indexPath.row])
         cell.myLabel.text = text[indexPath.row]
-        
-        
-        
-        
-        //let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        //cell.textLabel?.text = list[indexPath.row]
-        //cell.detailTextLabel?.text = list[indexPath.row]
-        //cell.imageView?.image = UIImage(named: "png")
+        cell.Telefon.text = number[indexPath.row]
         
         return(cell)
     }
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
